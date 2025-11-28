04282b8eb32ca0bd742cffb9d717b283bdae8bd6
Sacha <sachaobiedzy@gmail.com>
2025-11-28 16:43:23 +0100

    Refactor: changement du layout, ajout du dossier protected

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,12 @@
 import TempButton from '@components/TempButton';
 import Title from '@components/Title';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Index = () => {
     return (
         <SafeAreaView style={styles.container}>
-            <Title text="index" />
-            <TempButton text="Se connecter" to='/login' />
-            <TempButton text="S'enregistrer" to='/register' />
+            <Text>CHARGEMENT</Text>
         </SafeAreaView>
     );
 };
@@ -19,6 +17,7 @@ const styles = StyleSheet.create({
     container: {
         flex:1,
         alignItems: 'center',
+        justifyContent: 'center',
         rowGap: 20
     }
 });
